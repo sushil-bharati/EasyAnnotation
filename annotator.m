7b89fd7ec0d82513f13e797617e34d9cdc3e30ec
Sushil Pratap Bharati <sushil.bharati007@gmail.com>
2016-05-31 13:48:54 -0500

    Update annotator.m

--- annotator.m
+++ annotator.m
@@ -4,10 +4,10 @@
 % The code sucessfully extracts an image per widow and lets you manually annotate 
 % i.e. (manually drag a rectangular box on required object in an image) 
 % via rectangle. Right click then click on "Crop Image" when done. 
-% 'cord_test.txt' sucesfully records x,y,width and height of the bounding box drawn as you go along multiple images
+% 'cord_test.txt' sucesfully records x1,y1,x2(i.e. x1+width) and y2(i.e. y1+height) of the bounding box drawn as you go along multiple images
 
-contents = dir('C:\Images\*.jpg') % input your image dataset directory here with valid extension for image
-fileid = fopen('cord_test.txt','r+');% opens the file to write co-ordinates
+contents = dir('your_directory_containing_images') % input your image dataset directory here with valid extension for image
+fileid = fopen('ground_truth.txt','w+');% opens the file to write co-ordinates
 
 for i = 1 : numel(contents)
   filename = contents(i).name;
@@ -18,9 +18,10 @@ for i = 1 : numel(contents)
     I = imread(fname); %reads the image
 [I2,rect2] = imcrop(I); % records the co-ordinates, height and width of cropped image
 
-%writes x, y, width and height in text file
-fprintf(fileid,'%d %d %d %d\n',floor(rect2(1)),floor(rect2(2)),floor(rect2(3)),floor(rect2(4)));
+%writes in text file
+fprintf(fileid,'%d,%d,%d,%d\n',floor(rect2(1)),floor(rect2(2)),floor(rect2(1)+rect2(3)),floor(rect2(2)+rect2(4)));
 close;
 end
+
 
 
